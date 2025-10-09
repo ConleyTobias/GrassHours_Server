317fca4c16ea6ecdb54fffa9eefd9fc2d66f2e43
ConleyTobias <Conleytobias@outlook.com>
2025-10-09 16:06:05 -0500

    Updated UserData.json structure, doesn't work though

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+from math import nextafter
+
 from fastapi import FastAPI
 from utils import *
 import json
@@ -28,14 +30,22 @@ def login(username: str, password: str):
 
 @app.get("/signup/{Username}")
 def signup(username: str, password: str):
-    path = "Data/UserData.json"
-    with open(path, 'r') as file:
-        data = json.load(file)
+    with open("Data/UserData.json", 'r') as file:
+        UserData = json.load(file)
 
-    data["UserId"].append(data["LastUserId"])
-    data["LastUserId"] += 1
-    data["Username"].append(username)
-    data["Password"].append(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
-    data["GrassHours"].append(0)
-    data["Streak"].append(0)
-    data["LastLogged"].append("")
+    with open("Data/Usernames.json", 'r') as file:
+        Usernames = json.load(file)
+        
+    next_user_id = UserData["NextUserId"]
+    str_next_user_id = str(next_user_id)
+    UserData[str_next_user_id] = {}
+    UserData[str_next_user_id]["UserId"] = next_user_id
+    UserData[str_next_user_id]["Username"] = username
+    UserData[str_next_user_id]["Password"] = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    UserData[str_next_user_id]["GrassHours"] = 0
+    UserData[str_next_user_id]["Streak"] = 0
+    UserData[str_next_user_id]["LastLogged"] = ""
+    Usernames[username] = next_user_id
+    UserData["NextUserId"] += 1
+
+signup("John Doe", "123456")
