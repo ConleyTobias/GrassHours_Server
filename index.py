e869110cb8d09b4dbe66d40b4dcef91ab5f4b49a
ConleyTobias <Conleytobias@outlook.com>
2025-10-11 14:52:13 -0500

    Added untested admin commands

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from utils import *
 import bcrypt
+import datetime
 
 app = FastAPI()
 
@@ -8,8 +9,7 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World!"}
 
-
-@app.get("/login/{Username}")
+@app.get("/login/{Username}/{Password}")
 def login(username: str, entered_password: str):
     user_data = get_json("Data/UserData.json")
     username_data = get_json("Data/Usernames.json")
@@ -25,7 +25,7 @@ def login(username: str, entered_password: str):
         return {"message": "Incorrect Password"}
 
 
-@app.get("/signup/{Username}")
+@app.get("/signup/{Username}/{Password}")
 def signup(username: str, password: str):
     user_data = get_json("Data/UserData.json")
     usernames_data = get_json("Data/Usernames.json")
@@ -48,6 +48,7 @@ def signup(username: str, password: str):
             "GrassHours": 0,
             "Streak": 0,
             "LastLogged": "",
+            "Denied": []
         }
         usernames_data[username] = next_user_id
         user_data["NextUserId"] += 1
@@ -62,7 +63,7 @@ def signup(username: str, password: str):
 
 print(signup("new_2", "123456"))
 
-@app.get("/changePassword/{Username}")
+@app.get("/changePassword/{Username}/{CurrentPassword}/{NewPassword}")
 def change_password(username: str, current_password: str, new_password: str):
     user_data = get_json("Data/UserData.json")
     usernames_data = get_json("Data/Usernames.json")
@@ -76,3 +77,82 @@ def change_password(username: str, current_password: str, new_password: str):
         return {"message": "Change Password Successful"}
     else:
         return login(username, current_password)
+
+@app.get("/logData/{Username}/{Password}/{GrassHours}/{Description}")
+def add_log(username: str, password: str, grass_hours: int, description: str):
+    if login(username, password) == {"message": "Login Successful"}:
+        queue = get_json("Data/Queue.json")
+        next_id = str(queue["NextId"])
+        queue[next_id] = {"Date": datetime.today(), "Username": username, "GrassHours": grass_hours, "Description": description}
+        queue["NextId"] += 1
+
+        dump_json("Data/Queue.json", queue)
+
+@app.get("/getGrassHoursLeaderboard/top{number}")
+def get_grass_hours_leaderboard(number: int):
+    grass_hours_leaderboard = get_json("Data/GrassHoursLeaderboard.json")
+    return grass_hours_leaderboard[:number]
+
+@app.get("/getStreakLeaderboard/top{number}")
+def get_streak_leaderboard(number: int):
+    streak_leaderboard = get_json("Data/StreakLeaderboard.json")
+    return streak_leaderboard[:number]
+
+@app.get("/adminApprove/{id}/{EnteredAdminCode")
+def admin_approve(id: int, entered_admin_code: str):
+    admin_code = get_json("Data/AdminCode.json")
+    queue = get_json("Data/Queue.json")
+    user_data = get_json("Data/UserData.json")
+    log_data = get_json("Data/LogData.json")
+    usernames = get_json("Data/Usernames.json")
+
+    if not bcrypt.checkpw(entered_admin_code.encode('utf-8'), admin_code["Code"].encode('utf-8')):
+        return {"message": "Incorrect Password"}
+
+    if id in queue:
+        date = queue[id]["Date"]
+        username = queue[id]["Username"]
+        grass_hours = queue[id]["GrassHours"]
+        user_id = str(usernames[username])
+
+        if str(date)[:10] != user_data[user_id]["LastLogged"][:10]:
+            user_data[user_id]["LastLogged"] = date
+            user_data[user_id]["LastLogged"] = date
+
+        user_data[user_id]["GrassHours"] += grass_hours
+
+        log_data[username][id] = queue[id]
+        del queue[id]
+
+        dump_json("Data/Queue.json", queue)
+        dump_json("Data/UserData.json", user_data)
+        dump_json("Data/LogData.json", log_data)
+        return {"message": "Approve Successful"}
+
+    else:
+        return {"message": "Id not found"}
+
+@app.get("/adminDeny/{id}/{EnteredAdminCode}")
+def admin_deny(id: int, entered_admin_code: str):
+    user_data = get_json("Data/UserData.json")
+    queue = get_json("Data/Queue.json")
+    admin_code = get_json("Data/AdminCode.json")
+    usernames = get_json("Data/Usernames.json")
+
+    if not bcrypt.checkpw(entered_admin_code.encode('utf-8'), admin_code["Code"].encode('utf-8')):
+        return {"message": "Incorrect Password"}
+
+    username = queue[id]["Username"]
+    user_id = str(usernames[username])
+    user_data[user_id]["Denied"].append(id)
+    return {"message": "Deny Successful"}
+
+@app.get("/getQueue/{EnteredAdminCode}")
+def get_queue(entered_admin_code: str):
+    admin_code = get_json("Data/AdminCode.json")
+    if not bcrypt.checkpw(entered_admin_code.encode('utf-8'), admin_code["Code"].encode('utf-8')):
+        return {"message": "Incorrect Password"}
+
+    return get_json("Data/Queue.json")
+
+#TODO: test admin commands
--- utils.py
+++ utils.py
@@ -16,3 +16,30 @@ def get_json(path):
 def dump_json(path, data):
     with open(path, 'w') as file:
         json.dump(data, file, indent=4)
+
+def add_to_leaderboards(user_id, username, grass_hours, streak):
+    grass_hours_leaderboard = get_json("GrassHoursLeaderboard.json")
+    streak_leaderboard = get_json("StreakLeaderboard.json")
+
+    for i in range(grass_hours_leaderboard):
+        if grass_hours_leaderboard[i]["grass_hours"] > grass_hours:
+            pass
+        elif grass_hours_leaderboard[i]["grass_hours"] == grass_hours:
+            grass_hours_leaderboard.insert(i-1, {"user_id": user_id, "username": username, "grass_hours": grass_hours})
+            break
+        else:
+            grass_hours_leaderboard.insert(i, {"user_id": user_id, "username": username, "grass_hours": grass_hours})
+            break
+
+    for i in range(streak_leaderboard):
+        if streak_leaderboard[i]["streak"] > streak:
+            pass
+        elif streak_leaderboard[i]["streak"] == streak:
+            streak_leaderboard.insert(i - 1, {"user_id": user_id, "username": username, "streak": streak})
+            break
+        else:
+            streak_leaderboard.insert(i, {"user_id": user_id, "username": username, "streak": streak})
+            break
+
+        dump_json("StreakLeaderboard.json", streak_leaderboard)
+        dump_json("StreakLeaderboard.json", streak_leaderboard)
